b42d38aa47b6053ac405dcf2ecb778086508fa4c
Olga Rudina <krabee@mail.ru>
2022-10-23 23:34:44 +0400

    Add Homework_2

---
+++ Homework_2/Задача15/Program.cs
@@ -0,0 +1,17 @@
+﻿//Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+//6 -> да
+//7 -> да
+//1 -> нет
+void checkHoliday(int find)
+{
+	Console.WriteLine($"Задача №15");
+	if (find % 6 == 0 || find % 7 == 0)
+	{
+		Console.Write($"{find} -> да");	
+	}
+	else
+	{
+		Console.Write($"{find} -> нет");	
+	}
+}
+checkHoliday(new Random().Next(1, 8));
