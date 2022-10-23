d6bd4ef0036b0ed8b3c8dc490ae5adfa5436fb4b
Olga Rudina <krabee@mail.ru>
2022-10-23 22:26:17 +0400

    Add Homework_2

---
+++ Homework_2/Задача10/Program.cs
@@ -0,0 +1,14 @@
+﻿//Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа."
+//456 -> 5
+//782 -> 8
+//918 -> 1
+void getSecondDigit(int number)
+{
+	int result = number / 10 % 10;
+	
+	Console.WriteLine($"Задача №10");
+	Console.Write($"3x значное число: {number}, ");
+	Console.WriteLine($"вторая цифра: {result} ");
+}
+getSecondDigit(new Random().Next(100, 1000));
+Console.WriteLine();
