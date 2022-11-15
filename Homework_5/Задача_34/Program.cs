4aa19a9ad2a3224b1548935a3ba3711a9655cb3d
Olga Rudina <krabee@mail.ru>
2022-11-15 17:43:49 +0400

    Add homework_5

---
+++ Homework_5/Задача_34/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами.
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+
+int[] array = new int[7];
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(1, 1000);
+    Console.Write(array[i] + " ");
+}
+
+Console.WriteLine();
+
+int CountEven(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+        {
+            count++;
+        }
+    }
+
+    return count;
+}
+
+int result = CountEven(array);
+Console.WriteLine($"Количество четных чисел в массиве: {result}");
+
---
+++ Homework_5/Задача_36/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях.
+
+int[] array = new int[7];
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(1, 100);
+    Console.Write(array[i] + " ");
+}
+
+Console.WriteLine();
+
+int GetSum(int[] array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i % 2 == 1)
+        {
+            sum = sum + array[i];
+        }
+    }
+
+    return sum;
+}
+
+int result = GetSum (array);
+Console.WriteLine($"Сумма элементов на нечетных позициях = {result}");
+
+
