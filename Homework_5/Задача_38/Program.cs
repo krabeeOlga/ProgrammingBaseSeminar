50047d4c1624caca09fa1264249791e663610acb
Olga Rudina <krabee@mail.ru>
2022-11-15 17:41:35 +0400

    Add homework_5

---
+++ Homework_5/Задача_38/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задайте массив вещественных чисел.
+// Найдите разницу между максимальным и минимальным элементом массива.
+
+double[] array = new double[7];
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = (new Random().NextDouble() * 100) - 30;
+    Console.Write("{0,8:F2}", array[i]);
+}
+
+double GetDiff(double[] array)
+{
+    double max = array.Max();
+    double min = array.Min();
+    double diff = Math.Round((max - min), 2);
+    return diff;
+}
+
+Console.WriteLine();
+
+double result = GetDiff (array);
+Console.WriteLine($"Разница между максимальным и минимальным элементом массива = {result}");
