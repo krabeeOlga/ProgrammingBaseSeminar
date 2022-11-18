119f7a241f06645bc55c8e3758b2385cccd74056
Olga Rudina <krabee@mail.ru>
2022-11-18 15:03:45 +0400

    Add Homework

---
+++ Homework_6/Задача_41/Program.cs
@@ -0,0 +1,15 @@
+﻿// Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// new Random().Next(2, 5)
+
+int result = 0;
+
+for (int i = 1; i <= new Random().Next(3, 8); i++)
+{
+    Console.Write($"Введите число №{i}: ");
+    if (Convert.ToInt32(Console.ReadLine()) > 0) 
+    {
+        result++;
+    }
+} 
+Console.WriteLine($"Количество положительных чисел среди введенных: {result}");
---
+++ Homework_6/Задача_43/Program.cs
@@ -0,0 +1,35 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+// значения b1, k1, b2 и k2 задаются пользователем.
+
+Console.Write("Введите значение b1: ");
+int b1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите значение k1: ");
+int k1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите значение b2: ");
+int b2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите значение k2: ");
+int k2 = Convert.ToInt32(Console.ReadLine());
+
+
+
+double GetPointX (double b1, double k1, double b2, double k2)
+{
+    double x = (b2 - b1) / (k1 - k2);    
+    return x;
+}
+
+double GetPointY (double b1, double k1, double b2, double k2)
+{
+    double y = k2 * (b2 - b1) / (k1 - k2) + b2;
+    return y;
+}
+
+double X = GetPointX(b1, k1, b2, k2);
+double Y = GetPointY(b1, k1, b2, k2);
+
+Console.WriteLine("Точка пересечения двух прямых, заданных уравнениями");
+Console.Write($"y=k1*x+b1, y=k2*x+b2: ({Math.Round(X, 2)}; {Math.Round(Y, 2)})");
+//Console.Write("{0,8:F2}", X);
+
+
